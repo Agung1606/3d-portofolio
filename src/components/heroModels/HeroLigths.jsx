bc501dd46cfbcd8366c715b1412ef73ca3be7d20
Agung Saputra <agngsptra928@gmail.com>
2025-04-21 07:23:49 +0000

    first commit and 3D Models for Hero section is on progress

---
+++ src/components/heroModels/HeroLigths.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroLigths = () => {
+  return (
+    <>
+      <spotLight position={[2, 5, 6]} penumbra={0.2} intensity={100} angle={0.15} color={'white'} />
+    </>
+  )
+}
+
+export default HeroLigths
