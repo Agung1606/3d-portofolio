39139f5310b0a53d965a7f9aa87791db4f283f53
Agung Saputra <agngsptra928@gmail.com>
2025-05-02 13:46:38 +0000

    contact section ui

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FeatureCards from './sections/FeatureCards'
 import ExperienceSection from './sections/ExperienceSection'
 import TechStack from './sections/TechStack'
 import Testimonials from './sections/Testimonials'
+import Contact from './sections/Contact'
 
 const App = () => {
   return (
@@ -17,8 +18,9 @@ const App = () => {
       <LogoShowcase />
       <FeatureCards />
       <ExperienceSection />
-      <TechStack /> */}
-      <Testimonials />
+      <TechStack />
+      <Testimonials /> */}
+      <Contact />
     </>
   )
 }
